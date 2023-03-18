1e4cd0c10f8932324527278a6f478506fc9c1142
alex5667 <alex56671111@gmail.com>
2023-03-19 00:00:31 +0200

    feat: add style in notfound

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -6,9 +6,9 @@ import './NotFound.scss';
 const Error = () => {
   return (
     <div className="main__container">
-      <div className="main__content">
-        <h2 className="content__heading">Page Not Found</h2>
-        <p className="content__error">404</p>
+      <div className="main__content-not-found">
+        <h2 className="content__heading-not-found">Page Not Found</h2>
+        <p className="content__error-not-found">404</p>
         <ButtonLink to={HOME_ROUTE}>Back to Home</ButtonLink>
       </div>
     </div>
